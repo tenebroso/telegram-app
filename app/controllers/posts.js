5a5e0c0b75b26e2be2e2f545ef84d99ed2d5b20c
Jon Bukiewicz <jonathan@tenebroso.net>
2014-07-01 14:39:48 -0500

    add verification before the publish action can fire, and error message if no text has been added

--- app/controllers/posts.js
+++ app/controllers/posts.js
@@ -1,22 +1,35 @@
 export default Ember.ArrayController.extend({
 
 	postTextArea: '',
+	postFail: false,
 
 	actions: {
 
 		createPost: function() {
 
-			var post = this.store.createRecord('post', {
-				content: this.get('postTextArea'),
-				user: this.get('session.user'),
-				date: new Date()
-				//http://livsey.org/blog/2013/02/20/tick-tock/
-			});
-			
-			post.save().then(function(){
-				this.set('content', '');
-				window.console.log('postTextArea');
-			}); 
+			var postContent = this.get('postTextArea');
+			var controller = this;
+
+			var postFail = function() {
+				controller.setProperties({
+					postError:true
+				});
+			};
+
+			if(postContent) {
+				var post = this.store.createRecord('post', {
+					content: this.get('postTextArea'),
+					user: this.get('session.user'),
+					date: new Date()
+					//http://livsey.org/blog/2013/02/20/tick-tock/
+				});
+				post.save().then(function(){
+					controller.set('postTextArea','');
+					window.console.log('created post!');
+				});
+			} else {
+				postFail();
+			}
 
 		},
 
