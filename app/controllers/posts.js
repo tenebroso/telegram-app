fba7d18f96230dc0f91282c9ed15b1ee7d615dc0
Jon Bukiewicz <jonathan@tenebroso.net>
2014-07-01 11:40:05 -0500

    setup create post action, start of delete post action

--- app/controllers/posts.js
+++ app/controllers/posts.js
@@ -6,6 +6,25 @@ export default Ember.ArrayController.extend({
 
 	    createPost: function() {
 
+			var post = this.store.createRecord('post', {
+				content: this.get('postTextArea'),
+				user: this.get('session.user'),
+				date: new Date()
+				//http://livsey.org/blog/2013/02/20/tick-tock/
+			});
+			
+			post.save().then(function(){
+				self.set('content', '');
+				window.console.log('postTextArea');
+			}); 
+
+    	},
+
+    	//http://emberjs.com/guides/models/creating-and-deleting-records/
+    	deletePost: function() {
+
+			store.deleteRecord(post);
+
     	},
 
 		charactersLeft: function() {
