427b0fd260c1b2f67f23ff89d874db6912233251
Jon Bukiewicz <jonathan@tenebroso.net>
2014-06-30 21:36:36 -0500

    work on character count, adding post entry box, styling for post entry box

--- app/controllers/posts.js
+++ app/controllers/posts.js
@@ -1,12 +1,19 @@
 export default Ember.ArrayController.extend({
 
+	postTextArea: '',
+
 	actions: {
-	    
+
 	    createPost: function() {
 
+    	},
 
-    	}
-
+		charactersLeft: function() {
+			var remainChars = this.get('postTextArea').length();
+			var counter = 140 - remainChars;
+			return counter;
+			window.console.log('test!');
+		}.property('postTextArea'),
   	}
   
 });
