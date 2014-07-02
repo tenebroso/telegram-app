91108a6f44090ad5d2f6f37a9d3e018361cb492c
Jon Bukiewicz <jonathan@tenebroso.net>
2014-07-02 14:15:00 -0500

    setup follow and unfollow actions

--- app/controllers/user/followers.js
+++ app/controllers/user/followers.js
@@ -1,3 +1,15 @@
 export default Ember.ArrayController.extend({
 
+	followedByCurrentUser: false,
+
+	actions: {
+
+		followUser: function() {
+
+				followedByCurrentUser: true;
+				window.console.log('followed!');
+
+		}
+	}
+
 });
--- app/controllers/user/following.js
+++ app/controllers/user/following.js
@@ -1,3 +1,15 @@
 export default Ember.ArrayController.extend({
 
+	followUser: true,
+
+	actions: {
+
+		unFollowUser: function() {
+
+				followUser: false;
+				window.console.log('unfollowed!');
+
+		}
+	}
+
 });
