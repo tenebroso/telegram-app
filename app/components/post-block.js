b008bf79285684eaa00a41b8dee8b30ce93cd04e
Jon Bukiewicz <jonathan@tenebroso.net>
2014-07-04 09:57:45 -0500

    setup delete button to show/hide on posts

--- app/components/post-block.js
+++ app/components/post-block.js
@@ -1,4 +1,9 @@
 export default Ember.Component.extend({
+
+	isAuthor: function(){
+		return this.get('user') === this.get('post.user');
+	}.property('user', 'post.user'),
+
 	actions: {
 		delete: function() {
 			this.sendAction('delete', this.get('param'));
