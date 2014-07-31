57d2ca08a64610926d3cbd0b547b1a2808cf96a2
Jon Bukiewicz <jonathan@tenebroso.net>
2014-07-31 15:36:58 -0500

    attempts at saving user session in front-end

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,12 +2,12 @@ export default Ember.Route.extend({
 	beforeModel: function() {
 		var route = this;
 		var promise = this.store.find('user', {isAuthenticated: true});
-		return promise.then(function(users) {
+		promise.then(function(users) {
 			if (users && users.get('length') > 0) {
 				var user = users.get('firstObject');
 				route.set('session.user', user);
 			}
-			return users;
+			return user;
 		});
 	},
 	actions: {
