c6b574ff23fffa8155039c2afd8f7f708a539406
Jon Bukiewicz <jonathan@tenebroso.net>
2014-08-01 15:19:24 -0500

    fix moment issue by adding it as variable in function

--- app/helpers/format-date.js
+++ app/helpers/format-date.js
@@ -1,6 +1,6 @@
 //http://emberjs.com/guides/cookbook/user_interface_and_interaction/displaying_formatted_dates_with_moment_js/
 //http://emberjs.jsbin.com/iCaGUne/4/edit
-export default Ember.Handlebars.makeBoundHelper(function(date) {
+export default Ember.Handlebars.makeBoundHelper(function(date, moment) {
 	//var moment = ('moment');
 	return moment(date).fromNow();
 });
