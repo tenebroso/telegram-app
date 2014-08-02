c2a21c3906eb9958b23358b4c944534584ff162b
Jon Bukiewicz <jonathan@tenebroso.net>
2014-08-01 19:21:42 -0500

    fix for line 5

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,7 +2,7 @@ export default Ember.Route.extend({
 	beforeModel: function() {
 		var route = this;
 		var promise = this.store.find('user', {isAuthenticated: true});
-		promise.then(function(users) {
+		return promise.then(function(users) {
 			if (users && users.get('length') > 0) {
 				var user = users.get('firstObject');
 				route.set('session.user', user);
