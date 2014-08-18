c41d4ac4135863638024646c6ef18029e2dfd463
Jon Bukiewicz <jonathan@tenebroso.net>
2014-08-18 10:06:22 -0500

    Remove conflicting actions, since they exist in /routes/application.js

--- app/controllers/user/followers.js
+++ app/controllers/user/followers.js
@@ -2,14 +2,4 @@ export default Ember.ArrayController.extend({
 
 	followedByCurrentUser: false,
 
-	actions: {
-
-		followUser: function() {
-
-				//followedByCurrentUser: true;
-				window.console.log('followed!');
-
-		}
-	}
-
 });
--- app/controllers/user/following.js
+++ app/controllers/user/following.js
@@ -2,14 +2,4 @@ export default Ember.ArrayController.extend({
 
 	followUser: true,
 
-	actions: {
-
-		unFollowUser: function() {
-
-				//followUser: false;
-				window.console.log('unfollowed!');
-
-		}
-	}
-
 });
